d04c2f288ed9fae1d18cbe8207942224cac4f1b8
joedomabylv <joedomabylv@gmail.com>
2024-06-26 16:31:22 -0700

    add slock support

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #picom --experimental-backend -b
 picom -b
+xss-lock -- slock &
 xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --pos 3440x137 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 3440x1440 --pos 0x0 --rotate normal
